e09690c4639361a84928920adecb09089aa65565
Clint Checketts <clint.checketts@domo.com>
2017-03-20 14:24:06 -0600

    Use helloWorld var similar to docs

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,9 +1,4 @@
 #!/usr/bin/groovy
-def call(body) {
-  // evaluate the body block, and collect configuration into the object
-  def config = [:]
-  body.delegate = config
-  body()
-
-  echo "Hello World!"
+def call(String name = 'human') {
+  echo "Hello $name!"
 }
