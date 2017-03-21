1c06610e89938b12475e5226b8c0969b14609648
Clint Checketts <clint.checketts@domo.com>
2017-03-21 14:08:39 -0600

    Fix testng reporting

--- vars/multiGradlew.groovy
+++ vars/multiGradlew.groovy
@@ -36,7 +36,7 @@ def call(body) {
       } finally {
         junitResults
         //TestNG
-        step([$class: 'Publisher', reportFilenamePattern: '**/reports/tests/*.xml'])
+        step([$class: 'Publisher', reportFilenamePattern: '**/test-results/*.xml'])
       }
 
     }
