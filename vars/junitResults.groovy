c8dba5ad35de52a5da53436b7b1e2b8bea06b30c
Clint Checketts <clint.checketts@domo.com>
2017-03-21 09:48:46 -0600

    Make Junit global match gradle use case

--- vars/junitResults.groovy
+++ vars/junitResults.groovy
@@ -1,21 +1,4 @@
 #!/usr/bin/groovy
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.delegate = config
-    body()
-    def archiveTestResults = config.archiveTestResults ?: true
-
-    if (archiveTestResults) {
-        def surefire = new File ("target/surefire-reports")
-        if (surefire.exists() && surefire.isDirectory()) {
-            step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-        }
-
-        def failsafe = new File ("target/failsafe-reports")
-        if (failsafe.exists() && failsafe.isDirectory()) {
-            step([$class: 'JUnitResultArchiver', testResults: '**/target/failsafe-reports/TEST-*.xml'])
-        }
-    }
-
+def call(String resultsLocation = "**/test-results/*.xml") {
+    step([$class: 'JUnitResultArchiver', testResults: resultsLocation])
 }
